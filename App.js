f03ad9003daad1498e99f508fe8d7e27aa987b4e
divya_mishra <divyamishra075@gmail.com>
2023-01-22 00:26:54 +0530

    'parcel-webpack-config'

--- App.js
+++ App.js
@@ -1,6 +1,36 @@
-<html>
-    <head></head>
-    <body>
-        
-    </body>
-</html>
+/**
+ * parcel-
+ * hot module reloading depends upon - file watcher algorith written in c++ - parcel provides
+ * mified
+ * Bundling
+ * clean our code
+ * dev and production build
+ * super fast build algo
+ * image optimization
+ * caching while development, but local not the one coming from backend
+ * compress the files-like rename files
+ * compatible with older versions of browser-it add polyfills
+ * manages port number
+ * put parcel.cache in git ignore
+ * consistent caching algo, parcel uses
+ * manages port number
+ * zero config bundler
+ * 
+ * 
+ * --------
+ * 
+ * Transitive dependencies- parcel using some other dependencies, they are dependent on ther similarly
+ * 
+ * 
+ * -----
+ * 
+ *    check browserList.dev website, it makesures we the broswer it will support, here it will support all broswer we config it in package.json
+ */
+
+
+import React from "react";
+import ReactDom from "react-dom/client"
+
+const container = React.createElement( "h1", {id:"text"}, "heading1")
+const root = ReactDom.createRoot(document.getElementById("root"))
+root.render(container)
